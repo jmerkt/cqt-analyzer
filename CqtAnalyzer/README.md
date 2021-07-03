ba286b790e8b256522c7bfe562d2d6f133a26ca6
jmerkt <jmerkt190@gmail.com>
2021-07-03 22:46:35 +0200

    initial commit

---
+++ CqtAnalyzer/README.md
@@ -0,0 +1,2 @@
+# CqtAnalyzer
+A basic volume control effect plug-in with IGraphics GUI
---
+++ CqtAnalyzer/config.h
@@ -0,0 +1,54 @@
+#define PLUG_NAME "CqtAnalyzer"
+#define PLUG_MFR "ChromaDSP"
+#define PLUG_VERSION_HEX 0x00010000
+#define PLUG_VERSION_STR "1.0.0"
+#define PLUG_UNIQUE_ID 'FL7w'
+#define PLUG_MFR_ID 'Acme'
+#define PLUG_URL_STR "https://iplug2.github.io"
+#define PLUG_EMAIL_STR "spam@me.com"
+#define PLUG_COPYRIGHT_STR "Copyright 2020 Acme Inc"
+#define PLUG_CLASS_NAME CqtAnalyzer
+
+#define BUNDLE_NAME "CqtAnalyzer"
+#define BUNDLE_MFR "ChromaDSP"
+#define BUNDLE_DOMAIN "com"
+
+#define SHARED_RESOURCES_SUBPATH "CqtAnalyzer"
+
+#define PLUG_CHANNEL_IO "1-1 2-2"
+
+#define PLUG_LATENCY 0
+#define PLUG_TYPE 0
+#define PLUG_DOES_MIDI_IN 0
+#define PLUG_DOES_MIDI_OUT 0
+#define PLUG_DOES_MPE 0
+#define PLUG_DOES_STATE_CHUNKS 0
+#define PLUG_HAS_UI 1
+#define PLUG_WIDTH 600
+#define PLUG_HEIGHT 600
+#define PLUG_FPS 60
+#define PLUG_SHARED_RESOURCES 0
+#define PLUG_HOST_RESIZE 0
+
+#define AUV2_ENTRY CqtAnalyzer_Entry
+#define AUV2_ENTRY_STR "CqtAnalyzer_Entry"
+#define AUV2_FACTORY CqtAnalyzer_Factory
+#define AUV2_VIEW_CLASS CqtAnalyzer_View
+#define AUV2_VIEW_CLASS_STR "CqtAnalyzer_View"
+
+#define AAX_TYPE_IDS 'IEF1', 'IEF2'
+#define AAX_TYPE_IDS_AUDIOSUITE 'IEA1', 'IEA2'
+#define AAX_PLUG_MFR_STR "Acme"
+#define AAX_PLUG_NAME_STR "CqtAnalyzer\nIPEF"
+#define AAX_PLUG_CATEGORY_STR "Effect"
+#define AAX_DOES_AUDIOSUITE 1
+
+#define VST3_SUBCATEGORY "Fx"
+
+#define APP_NUM_CHANNELS 2
+#define APP_N_VECTOR_WAIT 0
+#define APP_MULT 1
+#define APP_COPY_AUV3 0
+#define APP_SIGNAL_VECTOR_SIZE 64
+
+#define ROBOTO_FN "Roboto-Regular.ttf"
